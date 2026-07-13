290b3f6d4ede03140fb55ea31455f6735942569a
0xb01 <johnnichols.ranara@gmail.com>
2026-07-13 15:27:20 +0800

    4182480

---
+++ 4182480.lua
@@ -0,0 +1,8 @@
+-- Smash H​​‌‌‌​​​​​‌‌​‌​‌​‌‌​​‌​‌​​‌‌​​‌​​​‌‌​​‌​​‌‌​​​​‌​​‌‌​​​‌​​‌‌​​‌‌​​‌‌‌​​​​​‌‌​​​​​‌‌​​‌​​​​‌‌​​​​​​‌‌​​‌‌​‌‌​​‌‌​​​‌‌​​​​​‌‌​​​​‌it Museum
+-- AppID 4182480 | Generated on 2026-07-13 07:25 UTC | openlua.cloud
+
+-- Main Application
+addappid(4182480)
+
+-- Content Depots (1)
+addappid(4182481, 1, "be37777c03f013823c95d376f0dbeb94096aa2713bfc0a4be91affb6eedda9f9") -- Smash Hit Museum - Content
