f0583df7e7f71f82f574e29d10ce5c07d464b380
0xb01 <johnnichols.ranara@gmail.com>
2026-07-16 10:10:26 +0800

    2569760

---
+++ 2569760.lua
@@ -0,0 +1,12 @@
+-- The Mound: O​​‌‌​​‌‌​​‌‌​​​‌​‌‌​​​‌​​​‌‌‌​​‌​‌‌​​‌‌​​​‌‌​​‌‌​​‌‌​​​‌​​‌‌‌​​‌​​‌‌​​​​​‌‌​​​‌​​​‌‌​​​‌​​‌‌‌​​​​​‌‌‌​​​​​‌‌​​‌‌​‌‌​​​​‌​​‌‌‌​​‌men of Cthulhu
+-- AppID 2569760 | Generated on 2026-07-15 11:30 UTC | openlua.cloud
+-- Note: This game has multiplayer features
+
+-- Main Application
+addappid(2569760)
+
+-- Content Depots (1)
+addappid(2569761, 1, "6fa06cc8ae41964f038a2f87b9961db2ca8d9316e83bc372ae328cf5adb8fe0b") -- The Mound: Omen of Cthulhu - Windows
+
+-- DLCs without Dedicated Depots (1)
+addappid(4630460) -- The Mound Omen of Cthulhu - Characters  Items Pack 
