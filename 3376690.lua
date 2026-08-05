1d9bef2a04a9bb622bc863ba47f5472d7699f89b
0xb01 <johnnichols.ranara@gmail.com>
2026-08-05 23:29:23 +0800

    3376690

---
+++ 3376690.lua
@@ -0,0 +1,9 @@
+-- THE HOUSE OF THE DEAD 2: Remake
+-- AppID 3376690 | Generated on 2026-07-29 05:37 UTC
+-- Note: This game has multiplayer features
+
+-- Main Application
+addappid(3376690)
+
+-- Content Depots (1)
+addappid(3376691, 1, "bb300f0659435ce2241c40d01d149835d706a1d843d04fbe39dd1c91037433ea") -- THE HOUSE OF THE DEAD 2: Remake - Content
