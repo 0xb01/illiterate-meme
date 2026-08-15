3dfbf0b438cf39999c055f2bbc1b3b48e7ba6cda
0xb01 <johnnichols.ranara@gmail.com>
2026-08-15 12:52:53 +0800

    3734290

---
+++ 3734290.lua
@@ -0,0 +1,12 @@
+-- Gobli​​‌‌​‌​​​​‌‌​‌‌‌​​‌‌​​‌​​​‌‌​​‌‌​‌‌​​‌‌​​‌‌​​​​‌​​‌‌​​‌​​​‌‌‌​​​​‌‌​​‌​‌​‌‌​​‌​‌​​‌‌‌​​‌​‌‌​​​‌‌​‌‌​​‌‌​​‌‌​​‌​‌​‌‌​​​‌‌​​‌‌​​​‌n Sushi
+-- AppID 3734290 | Generated on 2026-08-15 04:52 UTC | openlua.cloud
+
+-- Main Application
+addappid(3734290)
+
+-- Content Depots (1)
+addappid(3734291, 1, "7b30751b280f74a7e732f58866a6adc943fe30528093f0dbcea2705eea6e10f5") -- Goblin Sushi - Content
+
+-- DLCs with Content (1)
+addappid(4358410)
+addappid(4358411, 1, "bc3996046a35682c1ff1a6867a157c64b2c6d92c8ba8fb00aa612902312e34a0") -- Goblin Sushi - Supporter Pack
