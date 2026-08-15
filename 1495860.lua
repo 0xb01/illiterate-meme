a2e6c3fd5e63b35e6e080dd446a408bfbb4539eb
0xb01 <johnnichols.ranara@gmail.com>
2026-08-15 16:32:19 +0800

    1495860

---
+++ 1495860.lua
@@ -0,0 +1,11 @@
+-- What T​‌‌​​​​‌​‌‌​​​‌‌​​‌‌​​‌‌​​‌‌​‌‌​​​‌‌‌​​‌​​‌‌​​‌‌​​‌‌​​​‌​​‌‌​​​‌​​‌‌‌​​​​​‌‌​‌​‌​​‌‌​‌​‌​​‌‌​‌‌‌​‌‌​​‌​‌​‌‌​​‌‌​​‌‌​​‌‌​​‌‌​​​‌​he Dub?!
+-- AppID 1495860 | Generated on 2026-08-15 08:31 UTC | openlua.cloud
+-- Note: This game has multiplayer features
+-- Note: This game supports Steam Workshop content
+
+-- Main Application
+addappid(1495860, 1, "8c7eaa99dc456c37c5a3e6a3431a1921372e62f717749e35ae129176397b59a2")
+
+-- Content Depots (2)
+addappid(1495861, 1, "1a56a0867bce5f1f9c4c85af26e77b68188c81911638fa7ac16c0239a24df18a") -- What The Dub! Content
+addappid(1495862, 1, "1a0a6e187199abc9ceca0de2b6df1e3fc8484c71df4f56ba1a870da185fb1ec0") -- What The Dub?! Mac
