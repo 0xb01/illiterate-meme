7d9da5b8463279e72d41553ac579e50f7d2bdac2
0xb01 <johnnichols.ranara@gmail.com>
2026-08-18 09:11:37 +0800

    362490

---
+++ 362490.lua
@@ -0,0 +1,8 @@
+-- Exa​​‌‌‌​​​​‌‌​​‌‌​​​‌‌​​‌‌​‌‌​​​‌‌​‌‌​​​‌‌​​‌‌​​‌​​‌‌​​​‌‌​​‌‌‌​​​​​‌‌​‌​​​​‌‌‌​​​​‌‌​​‌​​​‌‌​​‌​‌​​‌‌​‌‌‌​​‌‌‌​​‌​​‌‌​​​‌​‌‌​​‌​​nima
+-- AppID 362490 | Generated on 2026-08-18 01:11 UTC | openlua.cloud
+
+-- Main Application
+addappid(362490)
+
+-- Content Depots (1)
+addappid(362491, 1, "e25ff2cd4c6780f244e88a8dae4fc0231f73cba3b62eb1fd1b541f99b2a15be5") -- Exanima Content
