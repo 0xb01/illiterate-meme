cf7b70acec2547ad84820ab9f51b2f2e22838a33
0xb01 <johnnichols.ranara@gmail.com>
2026-08-24 08:54:28 +0800

    3008740

---
+++ 3008740.lua
@@ -0,0 +1,8 @@
+-- Winter​​‌‌​​​​​​‌‌​‌​​​​‌‌​​​​​​‌‌‌​​​​​‌‌​‌‌‌​​‌‌​​​​​​‌‌​​​‌​‌‌​​‌​​​​‌‌‌​​‌​​‌‌​​‌​​​‌‌‌​​​​‌‌​​‌​‌​‌‌​​​‌​​​‌‌​​‌‌​‌‌​​‌‌​​​‌‌​​​​ Burrow
+-- AppID 3008740 | Generated on 2026-08-24 00:53 UTC | openlua.cloud
+
+-- Main Application
+addappid(3008740)
+
+-- Content Depots (1)
+addappid(3008741, 1, "f0bf5628ceba5807cf894814396eab56087604b28a59a1e298b1543ae0ce589e") -- Winter Burrow - Content
