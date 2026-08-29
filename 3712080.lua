9bb66eeb5eb78c10685937c268ced72ac410116e
0xb01 <johnnichols.ranara@gmail.com>
2026-08-29 13:01:34 +0800

    3712080

---
+++ 3712080.lua
@@ -0,0 +1,9 @@
+-- Funnel​​‌‌​​​‌​‌‌​​​‌​​​‌‌​​‌​​‌‌​​‌‌​​​‌‌‌​​‌​​‌‌‌​​​​‌‌​​​‌‌​​‌‌​​‌​​‌‌​​‌‌​​​‌‌​​‌​​‌‌​​‌​​​‌‌​​‌‌​​​‌‌​‌‌​​‌‌​​​‌​​‌‌​​​‌‌​‌‌​​‌​‌ Runners
+-- AppID 3712080 | Generated on 2026-08-28 18:30 UTC | openlua.cloud
+-- Note: This game has multiplayer features
+
+-- Main Application
+addappid(3712080)
+
+-- Content Depots (1)
+addappid(3712082, 1, "08653891068ae60dfcf4e6d57470f5146491abfe6631ac3b2be16e565be8513b") -- Funnel Runners - Windows
